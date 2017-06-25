90e9b1bfb184f66619251db046cfb317cc158b48
himeno-yusuke <himeno-yusuke@kayac.com>
2017-06-25 14:58:30 +0900

    firebaseと連携

--- src/js/lib/Firebase.js
+++ src/js/lib/Firebase.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import * as firebase from "firebase";
 import EventEmitter from "eventemitter3";
 
@@ -11,9 +12,10 @@ export default class Firebase extends EventEmitter {
       projectId: "ossan-532d3",
       storageBucket: "ossan-532d3.appspot.com",
       messagingSenderId: "69432145068",
-    }
+    };
     this.app = firebase.initializeApp(this._config);
     this._db = firebase.database();
+    this._store = {};
 
     this._initListener();
   }
@@ -21,16 +23,21 @@ export default class Firebase extends EventEmitter {
   static get EVENT() {
     return {
       PUSH: "push",
+      ADDED: "added",
     };
   }
 
   _initListener() {
+    this._ref = this._db.ref();
     this.on(Firebase.EVENT.PUSH, opts => {
-      this._db.ref().push({
+      this._ref.push({
         ossan_id: opts.ossan_id,
         lat: opts.lat,
         lng: opts.lng,
       });
+    });
+    this._ref.on("child_added", (data) => {
+      this.emit(Firebase.EVENT.ADDED, data.toJSON());
     });
   }
 }
